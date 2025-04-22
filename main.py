72d06f337d97b83e06d49881505dc5f158ed4714
melisacar <acarme17@itu.edu.tr>
2025-04-22 14:12:18 +0300

    fix: handle excel links with query params like ?0 in href

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def fetch_page(url):
         return None
 
 
-def get_latest_year_url(html, url):
+def get_year_urls(html):
     soup = BeautifulSoup(html, "html.parser")
     year_urls = []
 
@@ -20,10 +20,30 @@ def get_latest_year_url(html, url):
             full_url = urljoin(base_url, a["href"])
             year_urls.append(full_url)
     return year_urls
+
+def extract_excel_links(html):
+    year_urls = get_year_urls(html)
+    sorted_year_urls = sorted(year_urls, key=lambda x: int(x.split("/")[-1].split(".")[0]), reverse=True)
+    print(sorted_year_urls)
+    latest_year_url = sorted_year_urls[0] #### First html url
+    latest_year_content = fetch_page(latest_year_url)
+    soup = BeautifulSoup(latest_year_content, "html.parser")
     
+    excel_links = []
+    for a in soup.find_all("a", href=True):
+        href = a["href"] 
+        if ".xls" in href or ".xlsx" in href:
+            full_url = urljoin(base_url, href)
+            excel_links.append(full_url)
+    return excel_links
+
+
 ### main execution
 base_url = "https://yigm.ktb.gov.tr/"
 url = "https://yigm.ktb.gov.tr/TR-249704/aylik-bultenler.html"
 html_content = fetch_page(url)
 #print(html_content)
-print(get_latest_year_url(html_content, url))
+#print(get_year_urls(html_content, url))
+
+excel_links = extract_excel_links(html_content)
+print(excel_links)
