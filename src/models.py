929cdac1705ee10ce33c95406fcc33b821deaf73
melisacar <acarme17@itu.edu.tr>
2025-07-09 16:06:52 +0300

    feat: add models file for database schema

---
+++ src/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy import Column, Integer, Date, String, Float
+from sqlalchemy import UniqueConstraint
+from sqlalchemy import create_engine
+from config import DATABASE_URL
+
+Base = declarative_base()
+
+# Define the table
+class ist_sinir_kapilari_giris_yapan_vatandas(Base):
+    __tablename__ = 'ist_sinir_kapilari_giris_yapan_vatandas'
+    __table_args__ = (UniqueConstraint('tarih', 'sehir', 'sinir_kapilari', name = 'unique_ist_sinir_kapilari_giris_yapan_vatandas'),
+        {'schema': 'etl'}
+    )
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    tarih = Column(Date, nullable=False)
+    sehir = Column(String, nullable=False)
+    sinir_kapilari = Column(String, nullable=False)
+    vatandas_sayisi = Column(Float, nullable=True)
+    erisim_tarihi = Column(Date, nullable=False)
+
+engine = create_engine(DATABASE_URL)
+
+Session = sessionmaker(bind=engine)
